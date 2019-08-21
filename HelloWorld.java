e143fc9c324f0b84a0f93fda0892605e61c915e1
Pranav Bhaskar <pranavbhaskar17@gmail.com>
2019-08-21 15:42:59 +0530

    method

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,10 @@ class HelloWorld {
 	public static void main(String[] args) {
 		int x = Integer.parseInt(args[0]);
 		int y = Integer.parseInt(args[1]);
-		int z = x + y;
-		System.out.println("the sum is " + z);
+		System.out.println("the sum is " + HelloWorld.add(x,y));
 	} 
+	// add Method
+	static int add(int x, int y) {
+		return(x+y);
+	}
 }
