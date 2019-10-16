e068b5d680098f03906b628e2ef93f362c1f5fa5
Pranav Bhaskar <pranavbhaskar17@gmail.com>
2019-10-16 11:10:20 +0530

    writing into a file

--- patients.java
+++ patients.java
@@ -1,48 +1,48 @@
 import java.util.Scanner;
 import java.io.*;
 // class declaration
-public class BankUsers
+public class patients
 {
     public static void main(String[] args) throws IOException
     {
        Scanner sc = new Scanner(System.in);
        
        // array variable
-       BankUsers[] users = new BankUsers[5];
+       patients[] users = new patients[5];
        
        for(int i = 0; i < 5; i++){
            System.out.println("Enter Name: ");  
            String name = sc.next();
-           System.out.println("Enter Account Number: ");
-           long acc = sc.nextLong();
-           System.out.println("Enter Balance: ");
-           float bal = sc.nextFloat();
-           users[i] = new BankUsers(name, acc, bal);
+           System.out.println("Enter Age: ");
+           int age = sc.nextInt();
+           System.out.println("Enter Gender: ");
+           String gen = sc.next();
+           users[i] = new patients(name, age, gen);
        }
        System.out.println("Name\tAcc\t\tBal");
        System.out.println("---------------------------------------");
        for(int i = 0; i < 5; i++){
-        System.out.println(users[i].name + "\t" + users[i].acc + "\t" + users[i].bal);     
+        System.out.println(users[i].name + "\t" + users[i].age + "\t" + users[i].gen);     
 	    System.out.println("---------------------------------------");
        } 
          PrintWriter out = new PrintWriter(new FileWriter("OutFile.csv"));
-
+         out.println("Name,Age,Gender");
          for (int i = 0; i < 5; i++) {
              // The get(int) method throws IndexOutOfBoundsException, which must be caught.
-             out.println(users[i].name + "," + users[i].acc+ "," + users[i].bal);
+             out.println(users[i].name + "," + users[i].age+ "," + users[i].gen);
          }
          out.close();
     }
     // initialize variables
     String name;
-    long acc;
-    float bal;
+    int age;
+    String gen;
 
     // constructor declaration of class
-    public BankUsers(String name, long acc, float bal)
+    public patients(String name, int age, String gen)
     {
         this.name = name;
-        this.acc = acc;
-        this.bal = bal; 
+        this.age = age;
+        this.gen = gen;
     }
 }
