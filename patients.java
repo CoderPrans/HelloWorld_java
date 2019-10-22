043d82284c8a015f4b0b222db8a54fd077cf1460
Pranav Bhaskar <pranavbhaskar17@gmail.com>
2019-10-22 20:13:25 +0530

    update

--- patients.java
+++ patients.java
@@ -1,50 +1,53 @@
 import java.util.Scanner;
 import java.io.*;
-// class declaration
+
 public class patients
 {
-    public static void main(String[] args) throws IOException
-    {
-       Scanner sc = new Scanner(System.in);
-       
-       // array variable
-       patients[] users = new patients[5];
-       
-       for(int i = 0; i < 5; i++){
-           System.out.println("Enter Name: ");  
-           String name = sc.next();
-           System.out.println("Enter Age: ");
-           int age = sc.nextInt();
-           System.out.println("Enter Gender: ");
-           String gen = sc.next();
-           users[i] = new patients(name, age, gen);
-       }
-       System.out.println("Name\t| Age\t| Gender");
-       System.out.println("\n");
-       for(int i = 0; i < 5; i++){
-        System.out.println(users[i].name + "\t| " + users[i].age + "\t| " + users[i].gen);     
-	    System.out.println("---------------------------------------");
-       } 
-         PrintWriter out = new PrintWriter(new FileWriter("Patients_data.csv"));
-         out.println("Name,Age,Gender");
-         for (int i = 0; i < 5; i++) {
-             // The get(int) method throws IndexOutOfBoundsException, which must be caught.
-             out.println(users[i].name + "," + users[i].age+ "," + users[i].gen);
-         }
-         out.close();
-        System.out.println("Saved in Patients_data.csv file.");
-    }
-
-    // initialize variables
+    // variables
     String name;
     int age;
     String gen;
 
-    // constructor declaration of class
+    // constructor function
     public patients(String name, int age, String gen)
     {
         this.name = name;
         this.age = age;
         this.gen = gen;
+    }
+
+    // main function
+    public static void main(String[] args) throws IOException
+    {
+       Scanner sc = new Scanner(System.in);
+       
+       patients[] users = new patients[5];
+       
+       for(int i = 0; i < 5; i++){
+           System.out.println("Name Age Gender");  
+           String name = sc.next();
+           int age = sc.nextInt();
+           String gen = sc.next();
+           users[i] = new patients(name, age, gen);
+       }
+
+       System.out.println("\n");
+       System.out.println("Name\t| Age\t| Gender");
+       System.out.println("\n");
+
+       for(int i = 0; i < 5; i++){
+        System.out.println(users[i].name + "\t| " + users[i].age + "\t| " + users[i].gen);     
+	    System.out.println("---------------------");
+       } 
+      
+       // Printing into a csv file, appending if already exists. 
+       PrintWriter out = new PrintWriter(new FileWriter("Patients_data.csv", true));
+       //out.println("Name,Age,Gender");
+
+       for (int i = 0; i < 5; i++) {
+           out.println(users[i].name + "," + users[i].age+ "," + users[i].gen);
+       }
+       out.close();
+       System.out.println("Saved in Patients_data.csv file.");
     }
 }
